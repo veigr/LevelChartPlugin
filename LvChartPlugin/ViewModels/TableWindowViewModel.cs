2297b7950499075717cf6f5b97f681f5346b961a
veigr <veigr@cat-ears.net>
2015-04-30 15:09:40 +0900

    ・フィルター追加 ・10Lvごとの背景色変更を追加

--- LvChartPlugin/ViewModels/TableWindowViewModel.cs
+++ LvChartPlugin/ViewModels/TableWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Grabacr07.KanColleWrapper;
 using Grabacr07.KanColleWrapper.Models;
@@ -11,9 +12,9 @@ namespace LvChartPlugin.ViewModels
     {
 
         #region ShipTable変更通知プロパティ
-        private IEnumerable<ShipTableRow> _ShipTable;
+        private ShipTableRow[] _ShipTable;
 
-        public IEnumerable<ShipTableRow> ShipTable
+        public ShipTableRow[] ShipTable
         {
             get
             { return this._ShipTable; }
@@ -26,6 +27,97 @@ namespace LvChartPlugin.ViewModels
             }
         }
         #endregion
+
+
+        #region ShipTableSubHeader変更通知プロパティ
+        private ShipTableHeader _shipTableHeader;
+
+        public ShipTableHeader ShipTableHeader
+        {
+            get
+            { return this._shipTableHeader; }
+            set
+            { 
+                if (this._shipTableHeader == value)
+                    return;
+                this._shipTableHeader = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+
+        #region IsLocked変更通知プロパティ
+        private bool _IsLocked;
+
+        public bool IsLocked
+        {
+            get
+            { return this._IsLocked; }
+            set
+            {
+                if (this._IsLocked == value)
+                    return;
+                this._IsLocked = value;
+                this.RaisePropertyChanged();
+                this.UpdateView();
+                Properties.Settings.Default.IsLocked = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+        #endregion
+
+
+        #region LevelMaximum変更通知プロパティ
+        private int _LevelMaximum;
+
+        public int LevelMaximum
+        {
+            get
+            { return this._LevelMaximum; }
+            set
+            { 
+                if (this._LevelMaximum == value)
+                    return;
+                this._LevelMaximum = value;
+                this.RaisePropertyChanged();
+                this.UpdateView();
+                Properties.Settings.Default.LevelMaximum = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+        #endregion
+
+
+        #region LevelMinimum変更通知プロパティ
+        private int _LevelMinimum;
+
+        public int LevelMinimum
+        {
+            get
+            { return this._LevelMinimum; }
+            set
+            { 
+                if (this._LevelMinimum == value)
+                    return;
+                this._LevelMinimum = value;
+                this.RaisePropertyChanged();
+                this.UpdateView();
+                Properties.Settings.Default.LevelMinimum = value;
+                Properties.Settings.Default.Save();
+            }
+        }
+        #endregion
+
+
+        public TableWindowViewModel()
+        {
+            Properties.Settings.Default.Reload();
+            var settings = Properties.Settings.Default;
+            this._LevelMaximum = settings.LevelMaximum;
+            this._LevelMinimum = settings.LevelMinimum;
+            this._IsLocked = settings.IsLocked;
+        }
 
         public void Initialize()
         {
@@ -44,25 +136,44 @@ namespace LvChartPlugin.ViewModels
             if (!KanColleClient.Current.IsStarted) return;
 
             this.ShipTable = KanColleClient.Current.Homeport.Organization.Ships.Values
+                .Where(x => !this.IsLocked || x.IsLocked)
+                .Where(x => x.Level <= this.LevelMaximum)
+                .Where(x => this.LevelMinimum <= x.Level)
                 .GroupBy(x => x.Level)
                 .Select(x => new ShipTableRow
                 {
                     Lv = x.Key,
-                    Destroyer = x.Where(s => s.Info.ShipType.Id == 2),
-                    LightCruiser = x.Where(s => s.Info.ShipType.Id == 3),
-                    TorpedoCruiser = x.Where(s => s.Info.ShipType.Id == 4),
-                    HeavyCruiser = x.Where(s => s.Info.ShipType.Id == 5),
-                    AviationCruiser = x.Where(s => s.Info.ShipType.Id == 6),
-                    LightAircraftCarrier = x.Where(s => s.Info.ShipType.Id == 7),
-                    AircraftCarrier = x.Where(s => s.Info.ShipType.Id.Any(11, 18)),
-                    AviationBattleShip = x.Where(s => s.Info.ShipType.Id == 10),
-                    BattleCruiser = x.Where(s => s.Info.ShipType.Id == 8),
-                    BattleShip = x.Where(s => s.Info.ShipType.Id == 9),
-                    Submarine = x.Where(s => s.Info.ShipType.Id.Any(13, 14)),
-                    Other = x.Where(s => !s.Info.ShipType.Id.Any(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 18)),
+                    Destroyer = x.Where(s => s.Info.ShipType.Id == 2).ToDisplayStrings(),
+                    LightCruiser = x.Where(s => s.Info.ShipType.Id == 3).ToDisplayStrings(),
+                    TorpedoCruiser = x.Where(s => s.Info.ShipType.Id == 4).ToDisplayStrings(),
+                    HeavyCruiser = x.Where(s => s.Info.ShipType.Id == 5).ToDisplayStrings(),
+                    AviationCruiser = x.Where(s => s.Info.ShipType.Id == 6).ToDisplayStrings(),
+                    LightAircraftCarrier = x.Where(s => s.Info.ShipType.Id == 7).ToDisplayStrings(),
+                    AircraftCarrier = x.Where(s => s.Info.ShipType.Id.Any(11, 18)).ToDisplayStrings(),
+                    AviationBattleShip = x.Where(s => s.Info.ShipType.Id == 10).ToDisplayStrings(),
+                    BattleCruiser = x.Where(s => s.Info.ShipType.Id == 8).ToDisplayStrings(),
+                    BattleShip = x.Where(s => s.Info.ShipType.Id == 9).ToDisplayStrings(),
+                    Submarine = x.Where(s => s.Info.ShipType.Id.Any(13, 14)).ToDisplayStrings(),
+                    Other = x.Where(s => !s.Info.ShipType.Id.Any(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 18)).ToDisplayStrings(),
                 })
                 .OrderByDescending(x => x.Lv)
                 .ToArray();
+
+            this.ShipTableHeader = new ShipTableHeader
+            {
+                Destroyer = "駆逐艦\r\n" + this.ShipTable.SelectMany(x => x.Destroyer).ToSubHeader(),
+                LightCruiser = "軽巡洋艦\r\n" + this.ShipTable.SelectMany(x => x.LightCruiser).ToSubHeader(),
+                TorpedoCruiser = "重雷装巡洋艦\r\n" + this.ShipTable.SelectMany(x => x.TorpedoCruiser).ToSubHeader(),
+                HeavyCruiser = "重巡洋艦\r\n" + this.ShipTable.SelectMany(x => x.HeavyCruiser).ToSubHeader(),
+                AviationCruiser = "航空巡洋艦\r\n" + this.ShipTable.SelectMany(x => x.AviationCruiser).ToSubHeader(),
+                LightAircraftCarrier = "軽空母\r\n" + this.ShipTable.SelectMany(x => x.LightAircraftCarrier).ToSubHeader(),
+                AircraftCarrier = "正規・装甲空母\r\n" + this.ShipTable.SelectMany(x => x.AircraftCarrier).ToSubHeader(),
+                AviationBattleShip = "航空戦艦\r\n" + this.ShipTable.SelectMany(x => x.AviationBattleShip).ToSubHeader(),
+                BattleCruiser = "巡洋戦艦\r\n" + this.ShipTable.SelectMany(x => x.BattleCruiser).ToSubHeader(),
+                BattleShip = "戦艦\r\n" + this.ShipTable.SelectMany(x => x.BattleShip).ToSubHeader(),
+                Submarine = "潜水艦・潜水空母\r\n" + this.ShipTable.SelectMany(x => x.Submarine).ToSubHeader(),
+                Other = "その他\r\n" + this.ShipTable.SelectMany(x => x.Other).ToSubHeader(),
+            };
         }
     }
 
@@ -72,22 +183,53 @@ namespace LvChartPlugin.ViewModels
         {
             return values.Any(v => v == src);
         }
+
+        public static string ToDisplayString(this Ship ship)
+        {
+            return ship.Level + " " + ship.Info.Name;
+        }
+
+        public static string[] ToDisplayStrings(this IEnumerable<Ship> ships)
+        {
+            return ships.Select(s => s.ToDisplayString()).ToArray();
+        }
+
+        public static string ToSubHeader(this IEnumerable<string> ships)
+        {
+            return "(" + ships.Count() + " 隻)";
+        }
     }
 
     public class ShipTableRow
     {
         public int Lv { get; set; }
-        public IEnumerable<Ship> Destroyer { get; set; }
-        public IEnumerable<Ship> LightCruiser { get; set; }
-        public IEnumerable<Ship> TorpedoCruiser { get; set; }
-        public IEnumerable<Ship> HeavyCruiser { get; set; }
-        public IEnumerable<Ship> AviationCruiser { get; set; }
-        public IEnumerable<Ship> LightAircraftCarrier { get; set; }
-        public IEnumerable<Ship> AircraftCarrier { get; set; }
-        public IEnumerable<Ship> AviationBattleShip { get; set; }
-        public IEnumerable<Ship> BattleCruiser { get; set; }
-        public IEnumerable<Ship> BattleShip { get; set; }
-        public IEnumerable<Ship> Submarine { get; set; }
-        public IEnumerable<Ship> Other { get; set; }
+        public string[] Destroyer { get; set; }
+        public string[] LightCruiser { get; set; }
+        public string[] TorpedoCruiser { get; set; }
+        public string[] HeavyCruiser { get; set; }
+        public string[] AviationCruiser { get; set; }
+        public string[] LightAircraftCarrier { get; set; }
+        public string[] AircraftCarrier { get; set; }
+        public string[] AviationBattleShip { get; set; }
+        public string[] BattleCruiser { get; set; }
+        public string[] BattleShip { get; set; }
+        public string[] Submarine { get; set; }
+        public string[] Other { get; set; }
+    }
+
+    public class ShipTableHeader
+    {
+        public string Destroyer { get; set; }
+        public string LightCruiser { get; set; }
+        public string TorpedoCruiser { get; set; }
+        public string HeavyCruiser { get; set; }
+        public string AviationCruiser { get; set; }
+        public string LightAircraftCarrier { get; set; }
+        public string AircraftCarrier { get; set; }
+        public string AviationBattleShip { get; set; }
+        public string BattleCruiser { get; set; }
+        public string BattleShip { get; set; }
+        public string Submarine { get; set; }
+        public string Other { get; set; }
     }
 }
---
+++ LvChartPlugin/Views/Converters/LevelToColorConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using LvChartPlugin.ViewModels;
+
+namespace LvChartPlugin.Views.Converters
+{
+    public class LevelToColorConverter : IMultiValueConverter
+    {
+        public Brush Background1 { get; set; }
+        public Brush Background2 { get; set; }
+        private Brush CurrentBackground { get; set; }
+
+        public LevelToColorConverter()
+        {
+            this.Background1 = new SolidColorBrush(Colors.Transparent);
+            this.Background2 = new SolidColorBrush(Colors.Transparent);
+        }
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var defaultValue = new SolidColorBrush(Colors.Transparent);
+            if (values.Length != 2) return defaultValue;
+            if (!values.Any(x => x is ShipTableRow)) return defaultValue;
+
+            var current = values[0] as ShipTableRow;
+            var previous = values[1] as ShipTableRow;
+            var currentLv = current != null ? current.Lv : int.MaxValue;
+            var previousLv = previous != null ? previous.Lv : int.MaxValue;
+
+            if (previousLv == int.MaxValue) this.CurrentBackground = this.Background2;
+
+            if (currentLv / 10 != previousLv / 10) this.SwapBackground();
+
+            return this.CurrentBackground;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SwapBackground()
+        {
+            this.CurrentBackground = this.CurrentBackground.Equals(this.Background1)
+                ? this.Background2
+                : this.Background1;
+        }
+    }
+}
